27472d23dc6bea1534a251f81c030d1682f1ef0a
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2023-05-25 21:37:20 +1000

    fix stylesl

--- src/Components/repos/RepoItem.js
+++ src/Components/repos/RepoItem.js
@@ -3,7 +3,7 @@ import propTypes from 'prop-types'
 
 const RepoItem = ({ repo }) => {
     return (
-        <div className="card">
+        <div className="card btn-block text-left">
             <h3>
                 <a href={repo.html_url}>{repo.name}</a>
             </h3>
